dd957481258a3719c5e58263ccd007533f62dc45
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 16:47:07 +0700

    Menambahkan API movies

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,64 @@
+﻿using modul10_103022330036;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using modul10_103022330036;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.Xml;
+
+namespace MahasiswaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : ControllerBase
+    {
+        // Static list untuk menyimpan data Mahasiswa
+        private static List<Movie> movies = new List<Movie>
+        {
+            new Movie("The ShawHank  Redemption", "Frank Darabont", ["Bob Gunton", "Morgan Freeman", "Tim Robbins"] , "A banker convicted of uxoricide" +
+                " forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
+            new Movie("The GodFather", "Francis Ford Coppola", ["Marlon Brando", "Al Pacino", "James Caan"] , "The aging patriarch of an organized crime dynasty" +
+                " transfers control of his clandestine empire to his reluctant son."),
+            new Movie("The Dark Jnight", "Christopher Nolan", ["Christopher Nolan", "Heath Ledger", "Christian Bale"], "When a menace known as the Joker wreaks havoc and chaos on the people" +
+                " of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
+
+        };
+
+        // GET: api/mahasiswa
+        [HttpGet]
+        public ActionResult<List<Movie>> GetAll()
+        {
+            return movies;
+        }
+
+        // GET: api/movie/{index}
+        [HttpGet("{index}")]
+        public ActionResult<Movie> GetByIndex(int index)
+        {
+            if (index < 0 || index >= movies.Count)
+            {
+                return NotFound();
+            }
+            return movies[index];
+        }
+
+        // POST: api/movie
+        [HttpPost]
+        public ActionResult<List<Movie>> AddMahasiswa([FromBody] Movie moviee)
+        {
+            movies.Add(moviee);
+            return movies;
+        }
+
+        // DELETE: api/movie/{index}
+        [HttpDelete("{index}")]
+        public ActionResult<List<Movie>> DeleteMovie(int index)
+        {
+            if (index < 0 || index >= movies.Count)
+            {
+                return NotFound();
+            }
+            movies.RemoveAt(index);
+            return movies;
+        }
+    }
+}
--- obj/Debug/net8.0/modul10_103022330036.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul10_103022330036.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul10_103022330036")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88631f7b24b19cb31189f6652756d183113f2d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul10_103022330036")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul10_103022330036")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
